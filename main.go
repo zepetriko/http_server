4fe523bf6ab76582199ce52335cfa792d94a7a7b
joao_zampier <joaozampi98@gmail.com>
2026-05-18 16:28:38 -0300

    set middleware func and new handlers

--- main.go
+++ main.go
@@ -1,19 +1,62 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	w.Header().Set("Content=Type", "text/plain; charset=utf-8")
+
+	fmt.Fprintf(w, "Hits: %d", hits)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content=Type", "text/plain; charset=utf-8")
+
+	w.Write([]byte("Hits reset to 0"))
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
 
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
+
 	file_server := http.FileServer(http.Dir("."))
-	mux.Handle("/", file_server)
+	handler := http.StripPrefix("/app", file_server)
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err)
 	}
+
 }
